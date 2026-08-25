711d146bf07bcd16129e243d36b1abb3da9ec2c5
jayaPrakash21-jp <amanyu56531@gmail.com>
2026-08-25 22:23:44 +0530

    Time: 2 ms (99.95%) | Memory: 52.8 MB (22.09%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,55 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
-        ArrayList<Integer> jp = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        int n = nums.length;
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
 
-        // Count frequency
+        // 1. Find possible candidates
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
 
-        // Check frequency
-        for (int num : map.keySet()) {
-            if (map.get(num) > n / 3) {
-                jp.add(num);
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
 
-        return jp;
+        // 2. Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+
+            if (num == candidate2)
+                count2++;
+        }
+
+        // 3. Add actual majority elements
+        int n = nums.length;
+
+        if (count1 > n / 3)
+            ans.add(candidate1);
+
+        if (count2 > n / 3 && candidate2 != candidate1)
+            ans.add(candidate2);
+
+        return ans;
     }
 }
