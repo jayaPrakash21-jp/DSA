7819efd2cb8b4181d1a738397638ca5ad50d3846
jayaPrakash21-jp <amanyu56531@gmail.com>
2026-08-25 21:48:29 +0530

    Time: 1177 ms (5.00%) | Memory: 52.6 MB (53.17%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0;i<n;i++){
             int count=0;
             
-              for(int j=0;j<n;j++){
+              for(int j=i;j<n;j++){
                   if(nums[i]==nums[j]){
                     count++;
                   }
